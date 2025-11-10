3f6f53992e0d6d31234f9ddb66b98534c4b302ed
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-11-10 03:02:34 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,33 +1,14 @@
 #![no_std]
 #![no_main]
 
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+/// Entry point after setting page table to run in the higher half. 
+/// This function should never return!
+#[unsafe(no_mangle)]
+extern "C" fn _kmain(_hart_id: u64, _fdt_ptr: u64) -> ! {
     loop {}
 }
 
-#[inline]
-pub fn sbi_putchar(c: u8) {
-    // a0 = character (c)
-    // a6 = function ID (1 for PutChar)
-    // a7 = extension ID (1 for Console Extension)
-    unsafe {
-        core::arch::asm!(
-            "ecall",
-            in("a0") c as u64,
-            in("a6") 0x1u64,  // Function ID
-            in("a7") 0x1u64,  // Extension ID
-            options(nostack)
-        );
-    }
-}
-
-#[unsafe(no_mangle)]
-extern "C" fn _kmain() {
-    sbi_putchar(b'h');
-    sbi_putchar(b'e');
-    sbi_putchar(b'l');
-    sbi_putchar(b'l');
-    sbi_putchar(b'o');
-    loop{}
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
