4a3e400c40d9154da53603721e8e559d2d30dd84
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-11-10 02:56:51 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -6,7 +6,28 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+#[inline]
+pub fn sbi_putchar(c: u8) {
+    // a0 = character (c)
+    // a6 = function ID (1 for PutChar)
+    // a7 = extension ID (1 for Console Extension)
+    unsafe {
+        core::arch::asm!(
+            "ecall",
+            in("a0") c as u64,
+            in("a6") 0x1u64,  // Function ID
+            in("a7") 0x1u64,  // Extension ID
+            options(nostack)
+        );
+    }
+}
+
 #[unsafe(no_mangle)]
 extern "C" fn _kmain() {
+    sbi_putchar(b'h');
+    sbi_putchar(b'e');
+    sbi_putchar(b'l');
+    sbi_putchar(b'l');
+    sbi_putchar(b'o');
     loop{}
 }
