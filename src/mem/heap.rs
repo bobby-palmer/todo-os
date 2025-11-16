187d88aed4036093c17db502151ee8635e570f24
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-11-16 10:12:26 -0500

    THIS IS AN AUTO-GENERATED COMMIT BECAUSE I WAS TOO LAZY TO MAKE ONE

--- src/mem/heap.rs
+++ src/mem/heap.rs
@@ -1,184 +1,116 @@
-//! Global heap allocator that can be accessed via core::alloc to allocate in 
-//! kernel space
+use core::ptr::NonNull;
 
-use core::{alloc::{GlobalAlloc, Layout}, cmp::max, ptr::NonNull};
+use crate::mem::{address::Page, pfa};
 
-use spin::Mutex;
 
-use crate::mem::{page_list, Page};
-
-#[global_allocator]
-static GLOBAL_HEAP: KernelHeap = KernelHeap::new();
-
-/// General purpose global allocator that dispatches between slab and page 
-/// allocation
-struct KernelHeap {
-    caches: [Mutex<Cache>; 8],
-}
-
-impl KernelHeap {
-    const fn new() -> Self {
-        let caches = [
-            Mutex::new(Cache::new(Cache::MIN_ORDER + 0)),
-            Mutex::new(Cache::new(Cache::MIN_ORDER + 1)),
-            Mutex::new(Cache::new(Cache::MIN_ORDER + 2)),
-            Mutex::new(Cache::new(Cache::MIN_ORDER + 3)),
-            Mutex::new(Cache::new(Cache::MIN_ORDER + 4)),
-            Mutex::new(Cache::new(Cache::MIN_ORDER + 5)),
-            Mutex::new(Cache::new(Cache::MIN_ORDER + 6)),
-            Mutex::new(Cache::new(Cache::MIN_ORDER + 7)),
-        ];
-
-        Self {
-            caches,
-        }
-    }
-
-    /// get the potential "order" slab to allocate from
-    fn get_cache_order(layout: Layout) -> u32 {
-        let cache_bytes = max(layout.size(), layout.align());
-        let cache_order = cache_bytes.ilog2() +
-            if cache_bytes.is_power_of_two() { 0 } else { 1 };
-
-        max(cache_order, Cache::MIN_ORDER)
-    }
-}
-
-unsafe impl GlobalAlloc for KernelHeap {
-    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let order = Self::get_cache_order(layout);
-        let index = (order - Cache::MIN_ORDER) as usize;
-
-        if index < self.caches.len() {
-            todo!()
-        } else {
-            todo!() // Large allocation
-        }
-    }
-
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        let order = Self::get_cache_order(layout);
-        let index = (order - Cache::MIN_ORDER) as usize;
-
-        if index < self.caches.len() {
-            todo!()
-        } else {
-            todo!() // Large allocation
-        }
-    }
-}
-
-/// A collection of slabs
 struct Cache {
-    head: Option<NonNull<Slab>>,
-    order: u32,
+    order: usize,
+    slabs: Option<NonNull<Slab>>,
 }
 
 impl Cache {
-    // must have atleast enough for pointer in slots
-    const MIN_ORDER: u32 = 3;
-
-    const fn new(order: u32) -> Self {
+    const fn new(order: usize) -> Self {
         Self {
-            head: None,
             order,
+            slabs: None,
         }
     }
 
-    unsafe fn alloc(&mut self) -> Option<NonNull<u8>> {
+    fn alloc(&mut self) -> Option<NonNull<u8>> {
         unsafe {
-            let mut slab = self.head?;
-            let slot = slab.as_mut().alloc().unwrap();
-
-            if slab.as_ref().slots.0.is_none() {
-                self.head = slab.as_ref().next;
+            if self.slabs.is_none() {
+                self.prepend(Slab::new(1 << self.order)?);
             }
 
-            Some(slot)
+            let mut slab = self.slabs?;
+            let result = slab.as_mut().alloc();
+
+            if slab.as_ref().is_fully_taken() {
+                
+            }
+
+            result
         }
     }
 
-    unsafe fn dealloc(&mut self, ptr: NonNull<u8>) {
-        unsafe {
-            let mut slab = Slab::get_slab_for_ptr(ptr);
+    fn free(&mut self, ptr: NonNull<u8>) {
+        todo!()
+    }
 
-            if slab.as_ref().is_empty() {
-                slab.as_mut().next = self.head;
-                self.head = Some(slab);
-            }
-
-            slab.as_mut().dealloc(ptr);
-
-            if slab.as_ref().used_slots == 0 {
-                page_list::free(slab.cast());
-            }
-        }
+    fn prepend(&mut self, slab: NonNull<Slab>) {
+        todo!()
     }
 }
 
-/// A one page buffer that hands out buffers of "alloc_size" size
 struct Slab {
-    used_slots: usize,
     next: Option<NonNull<Self>>,
-    slots: SlabLink,
+    slots: Slot,
+    used: usize,
 }
 
 impl Slab {
-    unsafe fn get_slab_for_ptr(ptr: NonNull<u8>) -> NonNull<Self> {
-        let offset = ptr.align_offset(size_of::<Page>());
+    fn new(alloc_size: usize) -> Option<NonNull<Self>> {
         unsafe {
-            ptr.add(offset).sub(size_of::<Page>()).cast()
-        }
-    }
+            let page = pfa::alloc()?;
+            let mut me = page.cast::<Slab>();
 
-    unsafe fn new(alloc_size: usize) -> Option<NonNull<Self>> {
-        let page = page_list::alloc()?;
-
-        let mut me = page.cast::<Slab>();
-
-        unsafe {
-            me.write(Self {
-                used_slots: 0,
+            me.write(Slab {
                 next: None,
-                slots: SlabLink(None),
+                slots: Slot(None),
+                used: 0
             });
 
-            let after_header = me.add(1).cast::<u8>();
-            let mut slot_ptr = after_header.add(after_header.align_offset(alloc_size));
+            let mut slot = me.add(1).cast::<u8>();
 
-            while slot_ptr.add(alloc_size) < page.add(1).cast() {
-                let link = slot_ptr.cast::<SlabLink>();
-                link.write(me.as_ref().slots);
-                me.as_mut().slots.0 = Some(link);
-                slot_ptr = slot_ptr.add(alloc_size);
+            while slot.add(alloc_size) <= page.add(1).cast() {
+                me.as_mut().slots.prepend(slot.cast());
+                slot = slot.add(alloc_size);
             }
 
+            Some(me)
         }
-
-        Some(me)
     }
 
-    unsafe fn alloc(&mut self) -> Option<NonNull<u8>> {
-        let slot = self.slots.0?;
+    fn get_owner(ptr: NonNull<u8>) -> NonNull<Self> {
+        let offset = ptr.align_offset(size_of::<Page>());
 
-        unsafe { self.slots = slot.read(); }
-        self.used_slots += 1;
+        unsafe { 
+            ptr
+            .add(offset)
+            .sub(size_of::<Page>())
+            .cast()
+        }
+    }
 
+    fn alloc(&mut self) -> Option<NonNull<u8>> {
+        let slot = self.slots.pop()?;
+        self.used += 1;
         Some(slot.cast())
     }
 
-    unsafe fn dealloc(&mut self, ptr: NonNull<u8>) {
-        let node = ptr.cast::<SlabLink>();
-        unsafe { node.write(self.slots); }
-        self.slots.0 = Some(node);
-        self.used_slots -= 1;
+    fn free(&mut self, ptr: NonNull<u8>) {
+        self.slots.prepend(ptr.cast());
+        self.used -= 1;
     }
 
-    /// true -> no more empty slots
-    fn is_empty(&self) -> bool {
+    fn is_fully_taken(&self) -> bool {
         self.slots.0.is_none()
     }
+
 }
 
 #[derive(Clone, Copy)]
-struct SlabLink(Option<NonNull<Self>>);
+struct Slot(Option<NonNull<Self>>);
+
+impl Slot {
+    fn prepend(&mut self, slot: NonNull<Slot>) {
+        unsafe { slot.write(*self); }
+        self.0 = Some(slot);
+    }
+
+    fn pop(&mut self) -> Option<NonNull<Slot>> {
+        let node = self.0?;
+        unsafe {*self = node.read();}
+        Some(node)
+    }
+}
--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -1,9 +1,3 @@
+pub mod address;
+mod pfa;
 mod heap;
-mod page_list;
-
-/// Struct representation of a ram page
-#[repr(C)]
-#[repr(align(0x1000))]
-struct Page {
-    bytes: [u8; 0x1000]
-}
