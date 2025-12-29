ebdeb6d69ce7ac8934b190c6c2c0e8aad0aabefe
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-12-29 12:55:22 -0500

    reservations done

--- kernel/kmain.zig
+++ kernel/kmain.zig
@@ -1,9 +1,23 @@
 const std = @import("std");
 
 const DebugConsole = @import("DebugConsole.zig");
+const DeviceTree = @import("DeviceTree.zig");
 
 /// Boot hart entry point
-export fn kmain(_: usize, _: usize) noreturn {
+export fn kmain(_: usize, dtb: usize) noreturn {
+    var writer = DebugConsole.writer();
+
+    const device_tree = DeviceTree.parse(dtb) catch @panic("Device tree");
+
+    var reservations = device_tree.memory_reservations.iterator();
+
+    while (reservations.next()) |reservation| {
+        writer.print("start: {x}, size: {}\n", 
+            .{reservation.address, reservation.size}) catch unreachable;
+    }
+
+    writer.print("Done\n", .{}) catch unreachable;
+
     while (true) {}
 }
 
