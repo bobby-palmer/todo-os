c8b0da5782c34f3c09c71c04a54027fbcc39a55b
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-12-29 07:20:05 -0500

    debug console working

---
+++ kernel/DebugConsole.zig
@@ -0,0 +1,44 @@
+//! Early debug printing wrapper around open SBI
+
+const std = @import("std");
+const sbi = @import("sbi.zig");
+
+const Writer = std.io.Writer;
+const Error = Writer.Error;
+
+
+pub fn writer() Writer {
+    return Writer {
+        .vtable = &Writer.VTable {
+            .drain = drain,
+        },
+        // Un-buffered writer
+        .buffer = &.{},
+    };
+}
+
+fn drain(_: *Writer, data: []const []const u8, splat: usize) Error!usize {
+    var written: usize = 0;
+
+    for (0..data.len - 1)  |idx| {
+        for (data[idx]) |ch| {
+            _ = sbi.Legacy.console_putchar(ch) catch {
+                return Error.WriteFailed;
+            };
+
+            written += 1;
+        }
+    }
+
+    for (0..splat) |_| {
+        for (data[data.len - 1]) |ch| {
+            _ = sbi.Legacy.console_putchar(ch) catch {
+                return Error.WriteFailed;
+            };
+
+            written += 1;
+        }
+    }
+
+    return written;
+}
--- kernel/kmain.zig
+++ kernel/kmain.zig
@@ -1,7 +1,10 @@
-const sbi = @import("sbi.zig");
+const DebugConsole = @import("DebugConsole.zig");
 
 /// Boot hart entry point
 export fn kmain(_: u64, _: [*] const u64) noreturn {
-    sbi.Legacy.console_putchar('H') catch unreachable;
+    var writer = DebugConsole.writer();
+
+    writer.print("Hello {x}\n", .{0xd00dfeed}) catch unreachable;
+
     while (true) {} // TODO
 }
